0e6c5c6663d6cb057826530104ce120047de031f
bouzuya <m@bouzuya.net>
2023-08-28 23:04:08 +0900

    Add static file handler

--- crates/web/src/main.rs
+++ crates/web/src/main.rs
@@ -1,8 +1,11 @@
 use axum::{routing, Router, Server};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let app = Router::new().route("/", routing::get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .nest_service("/assets", ServeDir::new("./assets"))
+        .route("/", routing::get(|| async { "Hello, World!" }));
     Server::bind(&"0.0.0.0:3000".parse()?)
         .serve(app.into_make_service())
         .await?;
