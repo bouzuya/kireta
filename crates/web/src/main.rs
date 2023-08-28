652ca89ea9956fb1f54bb482efdf07f8d46416d9
bouzuya <m@bouzuya.net>
2023-08-28 22:51:45 +0900

    Add simple web server

--- crates/web/src/main.rs
+++ crates/web/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing, Router, Server};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new().route("/", routing::get(|| async { "Hello, World!" }));
+    Server::bind(&"0.0.0.0:3000".parse()?)
+        .serve(app.into_make_service())
+        .await?;
+    Ok(())
 }
