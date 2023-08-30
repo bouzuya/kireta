fcadca098160949bd20f9ea2a7aea66c720c19be
bouzuya <m@bouzuya.net>
2023-08-30 23:29:26 +0900

    Add counter example

--- android/App.tsx
+++ android/App.tsx
@@ -1,25 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import { useRef } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import WebView from "react-native-webview";
+import { useCallback, useRef, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import WebView, { WebViewMessageEvent } from "react-native-webview";
 import Constants from "expo-constants";
 
 export default function App() {
+  const [count, setCount] = useState<number>(0);
   const backendBaseUrl = Constants.expoConfig?.extra?.backendBaseUrl ?? null;
   if (backendBaseUrl === null)
     throw new Error("BACKEND_BASE_URL is not defined");
   const ref = useRef<WebView>(null);
   const uri = `${backendBaseUrl}/assets/index.html`;
+  const handleOnMessage = useCallback((event: WebViewMessageEvent): void => {
+    const message = JSON.parse(event.nativeEvent.data);
+    console.log("onMessage", message);
+    setCount(message.result);
+  }, []);
+  const handleOnPress = useCallback((): void => {
+    const message = {
+      name: "add",
+      args: [count, 1],
+    };
+    console.log("postMessage", message);
+    ref.current?.postMessage(JSON.stringify(message));
+  }, [count, ref]);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <WebView
         cacheEnabled={false}
+        containerStyle={styles.webView}
+        onMessage={handleOnMessage}
         ref={ref}
         source={{ uri }}
-        containerStyle={styles.webView}
       />
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{count}</Text>
+      <Button onPress={handleOnPress} title="Increment" />
     </View>
   );
 }
