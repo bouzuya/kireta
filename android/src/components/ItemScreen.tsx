f815be7d208125f4b8c6a5afecf5c8df8a11a2b0
bouzuya <m@bouzuya.net>
2023-09-11 21:42:26 +0900

    Add useItemScreen hook

--- android/src/components/ItemScreen.tsx
+++ android/src/components/ItemScreen.tsx
@@ -1,22 +1,52 @@
-import { StackActions } from "@react-navigation/native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
+import { useItemScreen } from "@/components/ItemScreen/hooks/useItemScreen";
 import type { NativeStackParamList } from "@/types/navigation";
 
 type Props = NativeStackScreenProps<NativeStackParamList, "Item">;
 
-export function ItemScreen({ navigation }: Props): JSX.Element {
+export function ItemScreen({
+  route: {
+    params: { itemId },
+  },
+}: Props): JSX.Element {
+  const { data, handleListItemOnPress } = useItemScreen(itemId);
+  if (data === null) {
+    return <Text>Loading...</Text>;
+  }
+  const { checkLists, days, item } = data;
   return (
     <View style={styles.container}>
-      <Button
-        onPress={() => {
-          navigation.dispatch(StackActions.push("List"));
-        }}
-      >
-        2023-09-06
-      </Button>
+      <View>
+        <Text>Item ID</Text>
+        <Text>{item.id}</Text>
+      </View>
+      <View>
+        <Text>Item name</Text>
+        <Text>{item.name}</Text>
+      </View>
+      <View>
+        <Text>Last purchased</Text>
+        <Text>{days !== null ? `${days} days ago` : "(none)"}</Text>
+      </View>
+      <View>
+        <Text>Number of purchases</Text>
+        <Text>{`${checkLists.length} times`}</Text>
+      </View>
+      <View>
+        <Text>Purchase history</Text>
+        <FlatList
+          data={checkLists}
+          keyExtractor={(checkList) => checkList.id}
+          renderItem={({ item: checkList }) => (
+            <Button onPress={handleListItemOnPress(checkList)}>
+              {checkList.date}
+            </Button>
+          )}
+        />
+      </View>
     </View>
   );
 }
---
+++ android/src/components/ItemScreen/hooks/useItemScreen.ts
@@ -0,0 +1,97 @@
+import { StackActions, useNavigation } from "@react-navigation/native";
+import { useCallback, useEffect, useState } from "react";
+import { useStore } from "@/components/StoreContext";
+import type { CheckList } from "@/types/check_list";
+import type { Item, ItemId } from "@/types/item";
+import {
+  findCheckedCheckListIdsByItemId,
+  findCheckList,
+  findItem,
+  type Store,
+} from "@/types/store";
+
+type ScreenState =
+  | {
+      itemId: ItemId;
+      type: "initial";
+    }
+  | {
+      checkLists: CheckList[];
+      days: number | null;
+      item: Item;
+      type: "loaded";
+    };
+
+export function useItemScreen(itemId: ItemId): {
+  data: {
+    checkLists: CheckList[];
+    days: number | null;
+    item: Item;
+  } | null;
+  handleListItemOnPress: (checkList: CheckList) => () => void;
+} {
+  const [screenState, setScreenState] = useState<ScreenState>({
+    itemId,
+    type: "initial",
+  });
+  const { store } = useStore();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setScreenState(handleScreenState(store, screenState));
+  }, [store, screenState]);
+
+  const handleListItemOnPress = useCallback(
+    (checkList: CheckList) => () => {
+      navigation.dispatch(
+        StackActions.push("List", { checkListId: checkList.id })
+      );
+    },
+    [navigation]
+  );
+  return {
+    data:
+      screenState.type === "loaded"
+        ? {
+            checkLists: screenState.checkLists,
+            days: screenState.days,
+            item: screenState.item,
+          }
+        : null,
+    handleListItemOnPress,
+  };
+}
+
+function handleScreenState(
+  store: Store,
+  screenState: ScreenState
+): ScreenState {
+  switch (screenState.type) {
+    case "initial": {
+      const item = findItem(store, screenState.itemId);
+      if (item === null) throw new Error("FIXME");
+      const checkListIds = findCheckedCheckListIdsByItemId(
+        store,
+        screenState.itemId
+      );
+      const checkLists = checkListIds
+        .map((id): CheckList | null => findCheckList(store, id))
+        .filter((checkList): checkList is CheckList => checkList !== null);
+      const days: number | null =
+        checkLists[0] === undefined
+          ? null
+          : ((new Date().getTime() -
+              new Date(checkLists[0].date + "T00:00:00Z").getTime()) /
+              (86400 * 1000)) |
+            0;
+      return {
+        checkLists,
+        days,
+        item,
+        type: "loaded",
+      };
+    }
+    default:
+      throw new Error("assert unknown screenState.type");
+  }
+}
