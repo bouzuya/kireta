dedc07d3b57513f7c82b8c4c641106a5de7dd181
bouzuya <m@bouzuya.net>
2023-10-21 09:41:26 +0900

    Add tests

--- crates/web/src/main.rs
+++ crates/web/src/main.rs
@@ -1,5 +1,7 @@
 mod model;
 mod query;
+#[cfg(test)]
+mod test_utils;
 
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
 use async_graphql_axum::GraphQL;
@@ -13,19 +15,67 @@ async fn graphiql() -> impl IntoResponse {
     Html(GraphiQLSource::build().endpoint("/graphql").finish())
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn route() -> Router {
     let schema = Schema::build(query::QueryRoot, EmptyMutation, EmptySubscription)
         .data(Store::example())
         .finish();
-    let app = Router::new()
+    Router::new()
         .route(
             "/graphql",
             routing::get(graphiql).post_service(GraphQL::new(schema)),
         )
-        .route("/", routing::get(|| async { "Hello, World!" }));
+        .route("/", routing::get(|| async { "Hello, World!" }))
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = route();
     Server::bind(&"0.0.0.0:3000".parse()?)
         .serve(app.into_make_service())
         .await?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_utils::{request, send_request, ResponseExt, StatusCode};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_hello() -> anyhow::Result<()> {
+        test_query(
+            r#"{"query":"query { hello }"}"#,
+            r#"{"data":{"hello":"Hello, World!"}}"#,
+        )
+        .await
+    }
+
+    #[tokio::test]
+    async fn test_add() -> anyhow::Result<()> {
+        test_query(
+            r#"{"query":"query { add(a: 1, b: 2) }"}"#,
+            r#"{"data":{"add":3}}"#,
+        )
+        .await
+    }
+
+    #[tokio::test]
+    async fn test_items() -> anyhow::Result<()> {
+        // dummy data
+        test_query(
+            r#"{"query":"query { items { id, name } }"}"#,
+            r#"{"data":{"items":[{"id":"1","name":"item1"},{"id":"2","name":"item2"}]}}"#,
+        )
+        .await
+    }
+
+    async fn test_query(query: &'static str, expected: &str) -> anyhow::Result<()> {
+        let app = route();
+        let request = request("POST", "/graphql", query)?;
+        let response = send_request(app, request).await?;
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(response.into_body_as_string().await?, expected);
+        Ok(())
+    }
 }
---
+++ crates/web/src/test_utils.rs
@@ -0,0 +1,36 @@
+pub use axum::http::Method;
+pub use axum::http::StatusCode;
+
+#[axum::async_trait]
+pub trait ResponseExt {
+    async fn into_body_as_string(self) -> anyhow::Result<String>;
+}
+
+#[axum::async_trait]
+impl ResponseExt for axum::response::Response<axum::body::BoxBody> {
+    async fn into_body_as_string(self) -> anyhow::Result<String> {
+        let body = self.into_body();
+        let bytes = hyper::body::to_bytes(body).await?.to_vec();
+        let s = String::from_utf8(bytes)?;
+        Ok(s)
+    }
+}
+
+pub fn request(
+    method: &str,
+    uri: &str,
+    body: &'static str,
+) -> Result<axum::http::Request<axum::body::Body>, axum::http::Error> {
+    axum::http::Request::builder()
+        .method(method)
+        .uri(uri)
+        .header(axum::http::header::CONTENT_TYPE, "application/json")
+        .body(axum::body::Body::from(body))
+}
+
+pub async fn send_request(
+    app: axum::Router,
+    request: axum::http::Request<axum::body::Body>,
+) -> Result<axum::response::Response<axum::body::BoxBody>, std::convert::Infallible> {
+    tower::ServiceExt::oneshot(app, request).await
+}
